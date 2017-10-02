508b0dabeb1c95604550be58e7747b3ba8ce005f
Mallory Spector <malloryspector@gmail.com>
2017-10-02 12:08:19 -0700

    Updated readme and file organization

--- README.md
+++ README.md
@@ -1,3 +1,49 @@
 ## HTML Parser
 
-Final project for CS5002 Discrete and Data Structures. Additional information comming soon!
+Final project for CS5002 Discrete and Data Structures. This project is an HTML parser, which takes in an html file and either outputs an error if tags are not being used correctly, or emulates their effect in text if tags are used correctly.
+
+Version 1.0 supports the following tags: <div>, <h1>, <h2>, <h3>, <p>, <br>, <img>, <b>, <i>, <ul>, <li>, <link>, <meta>.
+
+### How to Use It
+
+This program prompts the user for the desired HTML file to parse. There are three outcomes that can occur as long as the file input is valid.
+
+**Successful parsing** - If the file uses tags correctly, the program will then emulate the html file in the console. The parser and text emulation do take into account singleton tags (ones that don’t need a closing tag) and no attributes enclosed in a tag will be printed to the console.
+
+    > ./parser
+    Enter html file to parse: tests/success_test.html
+
+    -----------------------------
+     HTML has passed all tests!
+    ----------------------------
+
+    About — Constellation Calligraphy
+    - portfolio
+    - blog
+    - about
+    - contact
+
+
+**Failure comparing tags during parsing** - If the parser encounters an error while comparing the opening tag from the stack and the current closing tag, the parsing will stop completely and an error will be printed to console.
+
+    > ./parser
+    Enter html file to parse: tests/failure_compare.html
+
+    -------------------------
+     An error has been found
+    -------------------------
+
+    A tag is missing in the html file.
+
+
+**Failure after parsing** - If once the file has been totally parsed there are still opening tags left in the stack, an error will be printed to console with information about the missing closing tags.
+
+    > ./parser
+    Enter html file to parse: tests/failure_stack_remainder.html
+
+    --------------------------------------
+     The following errors have been found
+    --------------------------------------
+
+    Missing closing </li> tag.
+    Missing closing </b> tag.
--- main.c
+++ main.c
@@ -12,7 +12,7 @@
 #define SINGLETON_TAG 4
 
 /**
- * TAG CONSTANTS
+ * TAG ID CONSTANTS
  */
 #define H1 -1
 #define BOLD -2
@@ -20,26 +20,33 @@
 #define PARAGRAPH -4
 #define LIST_ITEM -5
 #define UNORDERED_LIST -6
-#define UNSUPPORTED_TAG -7 // catch all for any tag not yet supported
+#define LINK -7
+#define META -8
+#define IMAGE -9
+#define BREAK -10
+#define DIV -11
+#define H2 -12
+#define H3 -13
+#define UNSUPPORTED_TAG -14 // catch all for any tag not yet supported
 
-/** 
+/**
  * Struct to keep track of what is at the top of the stack
  */
 struct tag_stack {
 	struct tag *head;
 };
 
-/** 
+/**
  * Struct to keep track of the tag name and stack order
  */
 struct tag {
 	int tag_name;
-	int tag_type;
 	struct tag *next;
 };
 
-/* 
- * Returns TRUE if the stack has no elements 
+/*
+ * Returns TRUE if the stack has no elements
+ * Note: Function adapted from a class example
  */
 int is_empty(struct tag_stack *stack) {
     return stack == NULL || stack->head == NULL;
@@ -47,19 +54,18 @@ int is_empty(struct tag_stack *stack) {
 
 /**
  * Push the new tag name onto the tag stack
+ * Note: Function adapted from a class example
  */
-struct tag_stack *push(int tag_name, int tag_type, struct tag_stack *stack) {
+struct tag_stack *push(int tag_name, struct tag_stack *stack) {
 	if (is_empty(stack)) {
 		// if the tag stack is empty then this is the first element
 		stack->head = malloc(sizeof(struct tag));
 		stack->head->tag_name = tag_name;
-		stack->head->tag_type = tag_type;
 		stack->head->next = NULL;
 		return stack;
 	}
 	struct tag *new_element = malloc(sizeof(struct tag));
 	new_element->tag_name = tag_name;
-	new_element->tag_type = tag_type;
 	new_element->next = stack->head;
 	stack->head = new_element;
 	return stack;
@@ -67,6 +73,7 @@ struct tag_stack *push(int tag_name, int tag_type, struct tag_stack *stack) {
 
 /**
  * Returns the tag value on top of the stack and removes that tag from the stack.
+ * Note: Function adapted from a class example
  */
 struct tag *pop(struct tag_stack *stack) {
 	struct tag *to_return = stack->head;
@@ -78,6 +85,7 @@ struct tag *pop(struct tag_stack *stack) {
 
 /**
  * Free the entire stack before exiting the program.
+ * Note: Function adapted from a class example
  */
 void free_stack(struct tag_stack *stack) {
 	struct tag *t = stack->head;
@@ -89,22 +97,36 @@ void free_stack(struct tag_stack *stack) {
 	free(stack);
 }
 
+/**
+ * Compares the tag on top of the stack with the encountered closing tag
+ */
 int compare_tags(struct tag *t, int tag_name) {
 	if (t->tag_name == tag_name) {
 		return TRUE;
 	} else {
-		printf("--------------------------------------\n");
-		printf(" The following errors have been found \n");
-		printf("--------------------------------------\n\n");
-		printf("Missing tag.\n\n");
+		printf("\n-------------------------\n");
+		printf(" An error has been found \n");
+		printf("-------------------------\n\n");
+		printf("A tag is missing in the html file.\n\n");
 		return FALSE;
 	}
 }
 
+/**
+ * Helper for printing the emulated html
+ */
 void print_emulated_opening_tag(int tag) {
+	// Note: Looked at how to use switch statements for this project
+	// https://www.tutorialspoint.com/cprogramming/switch_statement_in_c.htm
 	switch(tag) {
 		case H1:
 			printf("#");
+			break;
+		case H2:
+			printf("##");
+			break;
+		case H3:
+			printf("###");
 			break;
 		case BOLD:
 			printf("*");
@@ -112,20 +134,26 @@ void print_emulated_opening_tag(int tag) {
       	case ITALIC:
       		printf("_");
       		break;
-      	case UNORDERED_LIST:	
+      	case UNORDERED_LIST:
       	case PARAGRAPH:
+      	case BREAK:
       		printf("\n");
       		break;
       	case LIST_ITEM:
-      		printf("-");
-      		break;	
+      		printf("- ");
+      		break;
       	default:
       		printf("");
       		break;
 	}
 }
 
+/**
+ * Helper for printing the emulated html
+ */
 void print_emulated_closing_tag(int tag) {
+	// Note: Looked at how to use switch statements for this project
+	// https://www.tutorialspoint.com/cprogramming/switch_statement_in_c.htm
 	switch(tag) {
 		case BOLD:
 			printf("*");
@@ -134,78 +162,166 @@ void print_emulated_closing_tag(int tag) {
       		printf("_");
       		break;
       	case H1:
+      	case H2:
+      	case H3:
       	case PARAGRAPH:
       	case UNORDERED_LIST:
       	case LIST_ITEM:
       		printf("\n");
-      		break;	
+      		break;
       	default:
       		printf("");
       		break;
-	}	
-}
-
-int get_tag_type(char tag_name[TAG_MAX_INPUT_SIZE]) {
-	char h1[] = "h1";
-	char bold[] = "b";
-	char italic[] = "i";
-	char paragraph[] = "p";
-	char list_item[] = "li";
-	char unordered_list[] = "ul";
-
-	if (strcmp(tag_name, h1) == 0) {
-		return H1;
-	} else if (strcmp(tag_name, bold) == 0) {
-		return BOLD;
-	} else if (strcmp(tag_name, italic) == 0) {
-		return ITALIC;
-	} else if (strcmp(tag_name, paragraph) == 0) {
-		return PARAGRAPH;
-	} else if (strcmp(tag_name, list_item) == 0) {
-		return LIST_ITEM;
-	} else if (strcmp(tag_name, unordered_list) == 0){
-		return UNORDERED_LIST;
-	} else {
-		return UNSUPPORTED_TAG;
 	}
 }
 
+/**
+ * Converts the tag name to the constant ID associated with the tag
+ */
+int get_tag_id(char tag_name[TAG_MAX_INPUT_SIZE]) {
+	if (strcmp(tag_name, "h1") == 0) {
+		return H1;
+	} else if (strcmp(tag_name, "b") == 0) {
+		return BOLD;
+	} else if (strcmp(tag_name, "i") == 0) {
+		return ITALIC;
+	} else if (strcmp(tag_name, "p") == 0) {
+		return PARAGRAPH;
+	} else if (strcmp(tag_name, "li") == 0) {
+		return LIST_ITEM;
+	} else if (strcmp(tag_name, "ul") == 0){
+		return UNORDERED_LIST;
+	} else if (strcmp(tag_name, "link") == 0) {
+		return LINK;
+	} else if (strcmp(tag_name, "meta") == 0) {
+		return META;
+	} else if (strcmp(tag_name, "img") == 0) {
+		return IMAGE;
+	} else if (strcmp(tag_name, "br") == 0) {
+		return BREAK;
+	} else if (strcmp(tag_name, "div") == 0) {
+		return DIV;
+	} else if (strcmp(tag_name, "h2") == 0) {
+		return H2;
+	} else if (strcmp(tag_name, "h3") == 0) {
+		return H3;
+	} else {
+		return UNSUPPORTED_TAG;
+	}
+
+}
+
+/*
+ * Determines if an opening tag is a singleton or a regular opening tag
+ */
+int get_tag_type(int tag_id) {
+	int tag_type;
+	// Note: Looked at how to use switch statements for this project
+	// https://www.tutorialspoint.com/cprogramming/switch_statement_in_c.htm
+	switch(tag_id) {
+		case LINK:
+		case META:
+		case IMAGE:
+		case BREAK:
+			tag_type = SINGLETON_TAG;
+			break;
+		default:
+			tag_type = OPENING_TAG;
+			break;
+	}
+	return tag_type;
+}
+
+/**
+ * Converts the tag constant ID to the tag name
+ */
+char * get_tag_name(int tag_name) {
+	if (tag_name == H1) {
+		return "h1";
+	} else if (tag_name == BOLD) {
+		return "b";
+	} else if (tag_name == ITALIC) {
+		return "i";
+	} else if (tag_name == PARAGRAPH) {
+		return "p";
+	} else if (tag_name == LIST_ITEM) {
+		return "li";
+	} else if (tag_name == UNORDERED_LIST) {
+		return "ul";
+	} else if (tag_name == LINK) {
+		return "link";
+	} else if (tag_name == META) {
+		return "meta";
+	} else if (tag_name == IMAGE) {
+		return "img";
+	} else if (tag_name == BREAK) {
+		return "br";
+	} else if (tag_name == DIV) {
+		return "div";
+	} else if (tag_name == H2) {
+		return "h2";
+	} else if (tag_name == H3) {
+		return "h3";
+	} else {
+		return "";
+	}
+}
+
+/**
+ * Emulates the HTML as text and prints the output to the console
+ */
 void print_html_as_text(char user_input[MAX_USER_INPUT]) {
-	printf("----------------------------\n");
+	printf("\n----------------------------\n");
 	printf(" HTML has passed all tests! \n");
 	printf("----------------------------\n\n");
 
 	int skip = FALSE;
 
+	// helper variables to keep track of each tag
+	char tag[10];
+	int t;
+
 	for (int i = 0; i < strlen(user_input) - 1; i++) {
-		char tag[10] = {0};
-		int t = 0;
 		int j = i + 1;
 
-		if (user_input[i] == '<') {
+		if (user_input[i] == '<' && user_input[j] == '!') {
+			// skip printing DOCTYPE and comment tags
+			skip = TRUE;
+			continue;
+		} else if (user_input[i] == '<') {
+			// clear the tag array so nothing is left over from previous
+			for (int k = 0; k < 10; k++) {
+				tag[k] = 0;
+			}
+			t = 0;
+
 			// get the tag type to emulate
 			while (user_input[j] != '>') {
 				if (user_input[j] == '/') {
 					j++;
+				} else if (user_input[j] == ' ') {
+					break;
 				} else {
 					tag[t] = user_input[j];
 					t++;
 					j++;
 				}
 			}
-			int tag_name = get_tag_type(tag);
+			int tag_id = get_tag_id(tag);
 
 			if (user_input[i + 1] != '/') {
-				print_emulated_opening_tag(tag_name);
+				print_emulated_opening_tag(tag_id);
 			} else {
-				print_emulated_closing_tag(tag_name);
+				print_emulated_closing_tag(tag_id);
 			}
 			skip = TRUE;
 		} else if (skip && user_input[i] != '>') {
 			continue;
 		} else if (user_input[i] == '>') {
 			skip = FALSE;
-		} else if (user_input[i] == '\t' || user_input[i] == '\n') {
+		} else if (user_input[i] == '\t' || user_input[i] == '\n' || user_input[i] == '\r' || user_input[i] == '\f') {
+			continue;
+		} else if (user_input[i] == ' ' && user_input[i - 1] == ' ') {
 			continue;
 		} else {
 			printf("%c", user_input[i]);
@@ -215,13 +331,18 @@ void print_html_as_text(char user_input[MAX_USER_INPUT]) {
 }
 
 void print_stack_remainder(struct tag_stack *stack) {
-	printf("--------------------------------------\n");
+	printf("\n--------------------------------------\n");
 	printf(" The following errors have been found \n");
 	printf("--------------------------------------\n\n");
 
 	while(!is_empty(stack)) {
 		struct tag *head = pop(stack);
-		printf("Missing closing </%d> tag.\n", head->tag_name);
+		char * tag_name = get_tag_name(head->tag_name);
+		if (head->tag_name != UNSUPPORTED_TAG) {
+			printf("Missing closing </%s> tag.\n", tag_name);
+		} else {
+			printf("Missing unsupported closing tag.\n");
+		}
 	}
 	printf("\n");
 }
@@ -229,8 +350,9 @@ void print_stack_remainder(struct tag_stack *stack) {
 int main() {
 
 	// get file for user input
+	// Note: Taking input from file was adapted from the in-class example
 	char input_to_parse[MAX_FILE_WEB_LENGTH];
-	printf("Enter file name: ");
+	printf("Enter html file to parse: ");
 	fgets(input_to_parse, MAX_FILE_WEB_LENGTH, stdin);
 
 	int len = strlen(input_to_parse);
@@ -263,35 +385,47 @@ int main() {
 	struct tag_stack *stack = malloc(sizeof(struct tag_stack));
 	stack->head = NULL;
 
+	// helper variables to keep track of each tag
+	char tag[10];
+	int t;
+
 	// begin parsing: loop through all user input
 	for (int i = 0; i < total_input_length - 1; i++) {
-		// helper variables to keep track of each tag
-		char tag[10] = {0};
-		int t = 0;
-
-		// helper variable to loop through user input without 
+		// helper variable to loop through user input without
 		// affecting the rest of the user input parsing
 		int j = i + 1;
 
-		if (user_input[i] == '<' && user_input[j] != '/') {
+		if (user_input[i] == '<' && user_input[j] != '/' && user_input[j] != '!') {
+			// clear the tag array so nothing is left over from previous
+			for (int k = 0; k < 10; k++) {
+				tag[k] = 0;
+			}
+			t = 0;
+
 			// if the opening '<' is not directly followed by a '/'
 			// then this is either an opening tag or a singleton tag
-			int tag_type = OPENING_TAG;
 			while (user_input[j] != '>') {
-				if (user_input[j] == '/') {
-					tag_type = SINGLETON_TAG;
+				if (user_input[j] == ' ') {
+					break;
 				}
 				tag[t] = user_input[j];
 				t++;
 				j++;
 			}
 
-			int tag_name = get_tag_type(tag);
+			int tag_id = get_tag_id(tag);
+			int tag_type = get_tag_type(tag_id);
 
 			if (tag_type == OPENING_TAG) {
-				stack = push(tag_name, tag_type, stack);
+				stack = push(tag_id, stack);
 			}
 		} else if (user_input[i] == '<' && user_input[j] == '/') {
+			// clear the tag array so nothing is left over from previous
+			for (int k = 0; k < 10; k++) {
+				tag[k] = 0;
+			}
+			t = 0;
+
 			// if the opening '<' is directly followed by a '/'
 			// then this is a closing tag
 			j++; // add one to skip adding the '/' with the tag name
@@ -301,10 +435,11 @@ int main() {
 				j++;
 			}
 
-			int tag_name = get_tag_type(tag);
+			int tag_id = get_tag_id(tag);
 
 			struct tag *head = pop(stack);
-			if (!compare_tags(head, tag_name)) {
+
+			if (!compare_tags(head, tag_id)) {
 				return 0;
 			}
 		}
@@ -323,22 +458,3 @@ int main() {
 	free_stack(stack);
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
